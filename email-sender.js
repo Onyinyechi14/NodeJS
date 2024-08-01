41eb7bdd793f1df3ccdd3050c03d1961d83fb117
Onyinyechi14 <giftjulius070@gmail.com>
2024-08-01 22:56:57 +0100

    working with js

---
+++ email-sender.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+// Create a transporter object
+const transporter = nodemailer.createTransport({
+  service: 'gmail', // or another service
+  auth: {
+    user: 'giftjulius070@gmail.com',
+    pass: 'giftjulius1993'
+  }
+});
+
+// Define email options
+const mailOptions = {
+  from: 'giftjulius070@gmail.com',
+  to: 'giftjulius070@gmail.com',
+  subject: 'Test Email',
+  text: 'Hello from Node.js!'
+};
+
+// Send email
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log('Error:', error);
+  } else {
+    console.log('Email sent:', info.response);
+  }
+});
